22f31c0166f26266e6d8ec4155e6fa548e1a2f8a
mfahad <fahad.azhar@bykea.com>
2021-09-02 10:58:39 +0500

    Seperate out fork user info in seperate hook

--- src/hooks/services/useFetchForkUser.tsx
+++ src/hooks/services/useFetchForkUser.tsx
@@ -1,26 +1,24 @@
-import { useQuery } from "react-query";
+import { useQuery } from 'react-query';
 
-import httpRequest from "../../config/service/HttpRequest";
+import httpRequest from '../../config/service/HttpRequest';
 
-type UserInfo = {
+export type UserInfo = {
   html_url: string;
+  updated_at: string;
   owner: {
     login: string;
     avatar_url: string;
-  }
-}
+  };
+};
 
 function fetchForkUserInfo(gistId: string): Promise<UserInfo[]> {
   return httpRequest({ method: 'GET', url: `/gists/${gistId}/forks` });
 }
 
 export default function useFetchForkUser(gistId: string) {
-  return useQuery(
-    [`fetchForkUserInfo-${gistId}`, gistId],
-    () => {
-      if (!gistId) return;
+  return useQuery([`fetchForkUserInfo-${gistId}`, gistId], () => {
+    if (!gistId) return;
 
-      return fetchForkUserInfo(gistId);
-    },
-  );
+    return fetchForkUserInfo(gistId);
+  });
 }
--- src/modules/UserGistSearch/components/ForkUserInfo.tsx
+++ src/modules/UserGistSearch/components/ForkUserInfo.tsx
@@ -1,7 +1,10 @@
-import { WithLoader } from "../../../components/WithLoader";
-import useFetchForkUser from "../../../hooks/services/useFetchForkUser";
+import { WithLoader } from '../../../components/WithLoader';
+import useFetchForkUser, { UserInfo } from '../../../hooks/services/useFetchForkUser';
 
 import './ForkUserInfo.scss';
+
+const sortByUpdatedAt = (previous: UserInfo, current: UserInfo) =>
+  new Date(previous.updated_at).getTime() - new Date(current.updated_at).getTime();
 
 export function ForkUserInfo({ gistId }: { gistId: string }) {
   const { data: forkedUserInfo, isFetching, error } = useFetchForkUser(gistId);
@@ -10,12 +13,15 @@ export function ForkUserInfo({ gistId }: { gistId: string }) {
     <WithLoader loading={isFetching} error={!!error}>
       <div className="ForkUserInfo_AvatarList AlignItemsCenter">
         {forkedUserInfo && forkedUserInfo.length > 0 && <h3>FORKED BY:</h3>}
-        {forkedUserInfo?.slice(0, 3).map(({ html_url, owner: { avatar_url, login } }) => (
-          <a key={login} href={html_url} className="Avatar" target="_blank" rel="noreferrer">
-            <img src={avatar_url} alt="avatar" />
-          </a>
-        ))}
+        {forkedUserInfo
+          ?.sort(sortByUpdatedAt)
+          .slice(0, 3)
+          .map(({ html_url, owner: { avatar_url, login } }) => (
+            <a key={html_url} href={html_url} className="Avatar" target="_blank" rel="noreferrer">
+              <img src={avatar_url} alt="avatar" />
+            </a>
+          ))}
       </div>
     </WithLoader>
-  )
+  );
 }
